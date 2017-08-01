6cabe69528e72a50eeabe2ed6c025be098f91521
Tom Foglio <tomcfoglio@users.noreply.github.com>
2017-08-01 16:46:06 +0100

    added errors to dictionary

--- dictionary/errorsService.js
+++ dictionary/errorsService.js
@@ -1,6 +1,11 @@
 const errors = {};
 
-errors['SERV_001'] = { statusCode: 500, name: 'ServiceNotAvailable', msg: 'This message can be overrided' };
+errors['SERV_001'] = { statusCode: 400, name: 'BadReference', msg: 'Reference does not exist' };
 
+errors['SERV_002'] = { statusCode: 404, name: 'NotFound', msg: 'Not Found' };
+
+errors['SERV_003'] = { statusCode: 409, name: 'Conflict', msg: 'This entity already exists' };
+
+errors['SERV_004'] = { statusCode: 500, name: 'ServiceNotAvailable', msg: 'This message can be overrided' };
 
 module.exports = errors;
