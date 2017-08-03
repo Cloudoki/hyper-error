69947e73b257bf722ba486c80d1f18f21d359d2d
Tom Foglio <tomcfoglio@users.noreply.github.com>
2017-08-03 11:11:13 +0100

    new errors

--- dictionary/errorsService.js
+++ dictionary/errorsService.js
@@ -6,6 +6,6 @@ errors['SERV_002'] = { statusCode: 404, name: 'NotFound', msg: 'Not Found' };
 
 errors['SERV_003'] = { statusCode: 409, name: 'Conflict', msg: 'This entity already exists' };
 
-errors['SERV_004'] = { statusCode: 500, name: 'ServiceNotAvailable', msg: 'This message can be overrided' };
+errors['SERV_004'] = { statusCode: 500, name: 'Internal', msg: 'Internal Server Error' };
 
 module.exports = errors;
