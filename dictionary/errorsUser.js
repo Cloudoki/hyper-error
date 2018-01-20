138f01e8cca2b51b2b699428f2aee5e2fa389f50
Tom Foglio <tomcfoglio@users.noreply.github.com>
2018-01-20 10:26:05 +0000

    added error message

--- dictionary/errorsUser.js
+++ dictionary/errorsUser.js
@@ -1,6 +1,6 @@
 const errors = {};
 
-errors['USR_003'] = { statusCode: 403, name: 'UserInvalidCredentials', msg: 'This message can be overrided' };
+errors['USR_003'] = { statusCode: 403, name: 'UserInvalidCredentials', msg: 'This user has no permission to access this resource.' };
 
 errors['USR_004'] = { statusCode: 401, name: 'ExpiredToken', msg: 'Recovery token has expired.' };
 
